8d2d08e3373c6c92c7cee25f5b9ae4b7fb8a623f
木头漂流 <1195668643@qq.com>
2025-04-20 09:12:34 +0800

    1

--- catvod/src/main/java/com/github/catvod/utils/Github.java
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -2,7 +2,7 @@ package com.github.catvod.utils;
 
 public class Github {
 
-    public static final String URL = "https://raw.githubusercontent.com/FongMi/Release/fongmi";
+    public static final String URL = "https://raw.githubusercontent.com/miaoqidong/Release/fongmi";
 
     private static String getUrl(String path, String name) {
         return URL + "/" + path + "/" + name;
